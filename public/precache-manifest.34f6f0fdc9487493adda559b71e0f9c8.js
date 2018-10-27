6fe173c306fb9d08eb922a9ad33ac5f929613ebb
Chad Edwards <chad@fergusapp.com>
2018-10-28 03:16:49 +1300

    Initial static serving of app

---
+++ public/precache-manifest.34f6f0fdc9487493adda559b71e0f9c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "105857ff4ff5bdde9259",
+    "url": "/static/js/main.105857ff.chunk.js"
+  },
+  {
+    "revision": "37f90d57916b981a0157",
+    "url": "/static/js/1.37f90d57.chunk.js"
+  },
+  {
+    "revision": "105857ff4ff5bdde9259",
+    "url": "/static/css/main.473e728d.chunk.css"
+  },
+  {
+    "revision": "657698e6d2d5cabc8da14669c4b73685",
+    "url": "/index.html"
+  }
+];
